56fbb5d43c493c5b9c053b6c8b33f43bc2909ce1
Daniele Coppola <dcoppola@ethz.ch>
2024-02-25 16:38:06 +0100

    comments, formatter, lsp

---
+++ after/plugin/defaults.lua
@@ -0,0 +1,9 @@
+vim.opt.relativenumber = true
+
+-- Telescope
+vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[S]earch [B]uffers' })
+vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
+
+vim.keymap.set('n', '<leader>l', require('lazy').sync, { desc = '[L]azy' })
+vim.opt.foldnestmax = 1
+vim.opt.foldmethod = 'indent'
